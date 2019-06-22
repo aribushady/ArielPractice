e20e7406c47e308e1854354f5d959d8e156aba06
aribushady <@lexander2008>
2019-06-22 11:34:14 -0400

    Fixed upload issue

---
+++ Ariel.py
@@ -0,0 +1,27 @@
+"""
+a = 2
+b = 3
+sum = a + b
+for i in range(sum):
+    if sum > 2:
+        sum += 3
+print(sum)
+"""
+
+def total(num1, num2):
+    total = num1 + num2
+    return
+
+for i in range(total):
+    if total > 9:
+        total += 2
+        print(total)
+
+    elif total < 9:
+        total -= 2
+        print(total)
+
+    else:
+        print("Oops")
+
+total(2, 3)
